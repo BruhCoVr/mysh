8a8d6faa09b43c67f7e0b933d682b5cf1c7f1856
TyW04 <tywagner04@gmail.com>
2026-09-18 17:05:16 -0600

    add more tests and change existing ones to reflect new library creation

--- tests/test_lib.c
+++ tests/test_lib.c
@@ -1,34 +1,40 @@
 #include "../include/lib.h"
+#include "../tlib/tlib.h"
 #include <stdio.h>
 
-#define TRUE 1
-#define FALSE 0
-
-#define ANSI_COLOR_RED     "\x1b[31m"
-#define ANSI_COLOR_GREEN   "\x1b[32m"
-#define ANSI_COLOR_RESET   "\x1b[0m"
-
-int success_state = TRUE;
-
-void TEST_ASSERT_EQUAL_UINT8(__uint8_t expected, __uint8_t actual) {
-    if (expected == actual) {
-        printf(ANSI_COLOR_GREEN "PASS\n" ANSI_COLOR_RESET);
-        return;
-    }
-    printf(ANSI_COLOR_RED "FAIL: %d does not equal %d\n" ANSI_COLOR_RESET, expected, actual);
-    success_state = FALSE;
+void test_str_len() {
+    TEST_ASSERT_EQUAL_UINT((uint8_t) 0, (uint8_t) str_len(""));
+    TEST_ASSERT_EQUAL_UINT((uint8_t) 5, (uint8_t) str_len("Hello"));
+    TEST_ASSERT_EQUAL_UINT((uint8_t) 255, (uint8_t) str_len("This is two hundred and fifty five characters of text and I am testing the str_len function to see if it will  return 255. Currently I have tested an empty string, five character string, and now a two hundred and fifty five character string. Wee wee Boo b"));
 }
 
-void test_str_len(void) {
-    printf("Testing str_len empty: ");
-    TEST_ASSERT_EQUAL_UINT8(0, str_len(""));
-    printf("Testing str_len of 5: ");
-    TEST_ASSERT_EQUAL_UINT8(5, str_len("Hello"));
-    printf("Testing str_len of 255 string: ");
-    TEST_ASSERT_EQUAL_UINT8(255, str_len("This is two hundred and fifty five characters of text and I am testing the str_len function to see if it will  return 255. Currently I have tested an empty string, five character string, and now a two hundred and fifty five character string. Wee wee Boo b"));
+void test_str_cmp() {
+    TEST_ASSERT_EQUAL_UINT((uint8_t) 0, (uint8_t) str_cmp("H", "Hi"));
+    TEST_ASSERT_EQUAL_UINT((uint8_t) 1, (uint8_t) str_cmp("Hi", "Hi"));
+    TEST_ASSERT_EQUAL_UINT((uint8_t) 1, (uint8_t) str_cmp("Hello : World!", "Hello : World!"));
 }
 
+void test_reverse() {
+    char test1[] = "test";
+    char test2[] = "hi";
+    char test3[] = "office chair";
+    TEST_ASSERT_EQUAL_CHAR_ARR("tset", reverse(test1));
+    TEST_ASSERT_EQUAL_CHAR_ARR("ih", reverse(test2));
+    TEST_ASSERT_EQUAL_CHAR_ARR("riahc eciffo", reverse(test3));
+}
+
+/*
+void test_digit_to_char() {
+
+}
+
+void test_itoa_int() {
+
+}
+*/
 int main(void) {
-    test_str_len();
-    return success_state;
+    RUN_TEST(test_str_len);
+    RUN_TEST(test_str_cmp);
+    RUN_TEST(test_reverse);
+    return 0;
 }
