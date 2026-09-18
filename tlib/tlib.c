6150ba7361082bf9993683877b993fc151bdbd54
TyW04 <tywagner04@gmail.com>
2026-09-18 17:03:21 -0600

    start of test library w/ INT, UINT, CHAR ARR functions

---
+++ tlib/tlib.c
@@ -0,0 +1,63 @@
+#include "tlib.h"
+#include <stdio.h>
+#include <string.h>
+
+int test_failure_count = 0;
+
+void assert_equal_uint8(uint8_t expected, uint8_t actual, const char* file, int line) {
+    if (expected != actual) {
+        printf(ANSI_COLOR_RED "[ASSERTION FAILED: UINT8 Mismatch] in %s at line %d\n" ANSI_COLOR_RESET, file, line);
+        printf("\tExpected " ANSI_COLOR_YELLOW "%u (0x%02X)" ANSI_COLOR_RESET " and got " ANSI_COLOR_YELLOW "%u (0x%02X)\n" ANSI_COLOR_RESET, expected, expected, actual, actual);
+        test_failure_count++;
+    }
+}
+
+void assert_equal_uint16(uint16_t expected, uint16_t actual, const char* file, int line) {
+    if (expected != actual) {
+        printf(ANSI_COLOR_RED "[ASSERTION FAILED: UINT16 Mismatch] in %s at line %d\n" ANSI_COLOR_RESET, file, line);
+        printf("\tExpected " ANSI_COLOR_YELLOW "%u (0x%04X)" ANSI_COLOR_RESET " and got " ANSI_COLOR_YELLOW "%u (0x%04X)\n" ANSI_COLOR_RESET, expected, expected, actual, actual);
+        test_failure_count++;
+    }
+}
+
+void assert_equal_uint32(uint32_t expected, uint32_t actual, const char* file, int line) {
+    if (expected != actual) {
+        printf(ANSI_COLOR_RED "[ASSERTION FAILED: UINT32 Mismatch] in %s at line %d\n" ANSI_COLOR_RESET, file, line);
+        printf("\tExpected " ANSI_COLOR_YELLOW "%u (0x%08X)" ANSI_COLOR_RESET " and got " ANSI_COLOR_YELLOW "%u (0x%08X)\n" ANSI_COLOR_RESET, expected, expected, actual, actual);
+        test_failure_count++;
+    }
+}
+
+void assert_equal_int8(int8_t expected, int8_t actual, const char* file, int line) {
+    if (expected != actual) {
+        printf(ANSI_COLOR_RED "[ASSERTION FAILED: INT8 Mismatch] in %s at line %d\n" ANSI_COLOR_RESET, file, line);
+        printf("\tExpected " ANSI_COLOR_YELLOW "%d (0x%02X)" ANSI_COLOR_RESET " and got " ANSI_COLOR_YELLOW "%d (0x%02X)\n" ANSI_COLOR_RESET, expected, expected, actual, actual);
+        test_failure_count++;
+    }
+}
+
+void assert_equal_int16(int16_t expected, int16_t actual, const char* file, int line) {
+    if (expected != actual) {
+        printf(ANSI_COLOR_RED "[ASSERTION FAILED: INT16 Mismatch] in %s at line %d\n" ANSI_COLOR_RESET, file, line);
+        printf("\tExpected " ANSI_COLOR_YELLOW "%d (0x%04x)" ANSI_COLOR_RESET " and got " ANSI_COLOR_YELLOW "%d (0x%04x)\n" ANSI_COLOR_RESET, expected, expected, actual, actual);
+        test_failure_count++;
+    }
+}
+
+void assert_equal_int32(int32_t expected, int32_t actual, const char* file, int line) {
+    if (expected != actual) {
+        printf(ANSI_COLOR_RED "[ASSERTION FAILED: INT32 Mismatch] in %s at line %d\n" ANSI_COLOR_RESET, file, line);
+        printf("\tExpected " ANSI_COLOR_YELLOW "%d (0x%08X)" ANSI_COLOR_RESET " and got " ANSI_COLOR_YELLOW "%d (0x%08X)\n" ANSI_COLOR_RESET, expected, expected, actual, actual);
+        test_failure_count++;
+    }
+}
+
+void assert_equal_char_arr(const char* expected, const char* actual, const char* file, int line) {
+    // Use strcmp function from string.h to test if equal
+    int areEqual = strcmp(expected, actual);
+    if (areEqual != 0) {
+        printf(ANSI_COLOR_RED "[ASSERTION FAILED: Char Array Mismatch] in %s at line %d\n" ANSI_COLOR_RESET, file, line);
+        printf("\tExpected " ANSI_COLOR_YELLOW "%s" ANSI_COLOR_RESET " and got " ANSI_COLOR_YELLOW "%s\n" ANSI_COLOR_RESET, expected, actual);
+        test_failure_count++;
+    }
+}
---
+++ tlib/tlib.h
@@ -0,0 +1,53 @@
+#ifndef TLIB_H
+#define TLIB_H
+
+#include <unistd.h>
+
+#define TRUE 1
+#define FALSE 0
+
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\e[0;33m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
+extern int test_failure_count;
+
+#define TEST_ASSERT_EQUAL_UINT(expected, actual) \
+    _Generic((expected), \
+        uint8_t: assert_equal_uint8, \
+        uint16_t: assert_equal_uint16, \
+        uint32_t: assert_equal_uint32 \
+    )((expected), (actual), __FILE__, __LINE__)
+
+#define TEST_ASSERT_EQUAL_INT(expected, actual) \
+    _Generic((expected), \
+        int8_t: assert_equal_int8, \
+        int16_t: assert_equal_int16, \
+        int32_t: assert_equal_int32 \
+    )((expected), (actual), __FILE__, __LINE__)
+
+void assert_equal_uint8(uint8_t expected, uint8_t actual, const char* file, int line);
+void assert_equal_uint16(uint16_t expected, uint16_t actual, const char* file, int line);
+void assert_equal_uint32(uint32_t expected, uint32_t actual, const char* file, int line);
+void assert_equal_int8(int8_t expected, int8_t actual, const char* file, int line);
+void assert_equal_int16(int16_t expected, int16_t actual, const char* file, int line);
+void assert_equal_int32(int32_t expected, int32_t actual, const char* file, int line);
+
+#define TEST_ASSERT_EQUAL_CHAR_ARR(expected, actual) \
+    assert_equal_char_arr((expected), (actual), __FILE__, __LINE__)
+
+void assert_equal_char_arr(const char* expected, const char* actual, const char* file, int line);
+
+#define RUN_TEST(test_function) do { \
+    int prev_fails = test_failure_count; \
+    printf("Running %s ... ", #test_function); \
+    test_function(); \
+    if (test_failure_count == prev_fails) { \
+        printf(ANSI_COLOR_GREEN "PASS\n" ANSI_COLOR_RESET); \
+    } else { \
+        printf(ANSI_COLOR_RED "FAIL\n" ANSI_COLOR_RESET); \
+    } \
+} while(0)
+
+#endif
