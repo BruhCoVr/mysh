74d3a29bf99b63f67ddcfadc64a994883e01a3e7
BruhCoVr <vgrin803@mtroyal.ca>
2026-09-18 21:37:37 -0600

    added the tests for itoa commiting to run on laptop cause screw windows

--- tests/test_lib.c
+++ tests/test_lib.c
@@ -23,14 +23,51 @@ void test_reverse() {
     TEST_ASSERT_EQUAL_CHAR_ARR("riahc eciffo", reverse(test3));
 }
 
+//test negative numbers and numbers larger than 20 characters
+void test_itoa_int(void) {
+    int num = 2345;
+    int neg_num = -9436;
+    int large_num = 1234567891234567892021;
+    char buffer[ITOA_BUFFER_SIZE];
+
+    printf("Testing integer to string: ");
+    TEST_ASSERT_EQUAL_CHAR_ARR("2345", itoa(num, buffer));
+    TEST_ASSERT_EQUAL_CHAR_ARR("-9436", itoa(neg_num, buffer));
+    TEST_ASSERT_EQUAL_CHAR_ARR("1234567891234567892021", itoa(large_num, buffer));
+
+}
+
+void test_itoa_sizet(void) {
+    size_t num = 2345;
+    int neg_num = -9436;
+    int large_num = 1234567891234567892021;
+    char buffer[ITOA_BUFFER_SIZE];
+
+    printf("Testing size t to string: ");
+    TEST_ASSERT_EQUAL_CHAR_ARR("2345", itoa(num, buffer));
+    TEST_ASSERT_EQUAL_CHAR_ARR("-9436", itoa(neg_num, buffer));
+    TEST_ASSERT_EQUAL_CHAR_ARR("1234567891234567892021", itoa(large_num, buffer));
+
+}
+
+void test_itoa_ssizet(void) {
+    ssize_t num = 2345;
+    int neg_num = -9436;
+    int large_num = 1234567891234567892021;
+    char buffer[ITOA_BUFFER_SIZE];
+
+    printf("Testing ssize t to string: ");
+    TEST_ASSERT_EQUAL_CHAR_ARR("2345", itoa(num, buffer));
+    TEST_ASSERT_EQUAL_CHAR_ARR("-9436", itoa(neg_num, buffer));
+    TEST_ASSERT_EQUAL_CHAR_ARR("1234567891234567892021", itoa(large_num, buffer));
+
+}
+
 /*
 void test_digit_to_char() {
 
 }
 
-void test_itoa_int() {
-
-}
 */
 int main(void) {
     RUN_TEST(test_str_len);
